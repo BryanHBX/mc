6b06dd370e1937ae60c0979b86c114e05e6af0a3
marco <z405656232x@163.com>
2018-04-07 23:01:32 +0800

    database updated

--- mc.sql
+++ mc.sql
@@ -11,35 +11,11 @@
  Target Server Version : 50553
  File Encoding         : 65001
 
- Date: 03/04/2018 23:43:35
+ Date: 07/04/2018 23:01:10
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for t_contract
--- ----------------------------
-DROP TABLE IF EXISTS `t_contract`;
-CREATE TABLE `t_contract`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contract_no` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `student_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for t_invoice
--- ----------------------------
-DROP TABLE IF EXISTS `t_invoice`;
-CREATE TABLE `t_invoice`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contract_id` int(11) NULL DEFAULT NULL,
-  `invoice_no` int(11) NULL DEFAULT NULL,
-  `pay_money` double NULL DEFAULT NULL,
-  `pay_type` tinyint(4) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
 -- Table structure for t_member
@@ -67,26 +43,15 @@ CREATE TABLE `t_member_role`  (
 ) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_role
--- ----------------------------
-DROP TABLE IF EXISTS `t_role`;
-CREATE TABLE `t_role`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `role_id` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
 -- Table structure for t_school
 -- ----------------------------
 DROP TABLE IF EXISTS `t_school`;
 CREATE TABLE `t_school`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `school_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `school_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `school_contact` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `status` tinyint(4) NULL DEFAULT NULL,
+  `s_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `s_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `s_contact` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `s_status` tinyint(4) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -94,6 +59,30 @@ CREATE TABLE `t_school`  (
 -- Records of t_school
 -- ----------------------------
 INSERT INTO `t_school` VALUES (1, 'string', 'string', 'string', 0);
+
+-- ----------------------------
+-- Table structure for t_school_contract
+-- ----------------------------
+DROP TABLE IF EXISTS `t_school_contract`;
+CREATE TABLE `t_school_contract`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contract_no` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `student_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_school_invoice
+-- ----------------------------
+DROP TABLE IF EXISTS `t_school_invoice`;
+CREATE TABLE `t_school_invoice`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contract_id` int(11) NULL DEFAULT NULL,
+  `invoice_no` int(11) NULL DEFAULT NULL,
+  `pay_money` double NULL DEFAULT NULL,
+  `pay_type` tinyint(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
 -- Table structure for t_school_product
@@ -111,10 +100,10 @@ CREATE TABLE `t_school_product`  (
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_student
+-- Table structure for t_school_student
 -- ----------------------------
-DROP TABLE IF EXISTS `t_student`;
-CREATE TABLE `t_student`  (
+DROP TABLE IF EXISTS `t_school_student`;
+CREATE TABLE `t_school_student`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `s_name` varchar(120) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `s_wxid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -123,6 +112,60 @@ CREATE TABLE `t_student`  (
   `s_contactPhone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `s_meto` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `memberId` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_sys_config
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_config`;
+CREATE TABLE `t_sys_config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `c_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `c_single` tinyint(4) NULL DEFAULT NULL,
+  `c_parent` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 26 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_config
+-- ----------------------------
+INSERT INTO `t_sys_config` VALUES (24, '中国舞', '中国舞', NULL, 22);
+INSERT INTO `t_sys_config` VALUES (23, '拉丁舞', '拉丁舞', NULL, 22);
+INSERT INTO `t_sys_config` VALUES (21, '种类', '种类', NULL, 18);
+INSERT INTO `t_sys_config` VALUES (25, 'C_LEVEL', '课程年段', 0, NULL);
+INSERT INTO `t_sys_config` VALUES (22, '舞蹈', '舞蹈', NULL, 21);
+INSERT INTO `t_sys_config` VALUES (16, 'C_TYPE', '课程种类', 0, NULL);
+INSERT INTO `t_sys_config` VALUES (17, '学科辅导', '学科辅导', 0, 16);
+INSERT INTO `t_sys_config` VALUES (18, '艺术体育', '艺术体育', 0, 16);
+
+-- ----------------------------
+-- Table structure for t_sys_config_val
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_config_val`;
+CREATE TABLE `t_sys_config_val`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_id` int(11) NOT NULL,
+  `c_value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `c_id`(`c_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_config_val
+-- ----------------------------
+INSERT INTO `t_sys_config_val` VALUES (3, 4, 'ab');
+INSERT INTO `t_sys_config_val` VALUES (4, 4, 'e1fg');
+
+-- ----------------------------
+-- Table structure for t_sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_role`;
+CREATE TABLE `t_sys_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `role_id` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
